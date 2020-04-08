ef2c5cabc4c11b8e49cdcc7dd10b82e87a10780b
Jesse Langley <jlangleyiii@aol.com>
2020-04-08 02:18:17 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
-count = 0
+magic_exit_number = 0
+count = 10
+while count < 11 && count != magic_exit_number do
+  puts " #{count} "
+  count -= 1
+end
+if count == 0 then puts "Happy New Year!"
+end
